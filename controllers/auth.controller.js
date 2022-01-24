af1db035d0aeaccaf8c41e588ad3d3d8c768832f
jonathanm98 <jonathanmenelet@hotmail.fr>
2022-01-24 02:02:44 +0100

    Ajout authentification + creation token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,34 @@
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN, {
+    expiresIn: 1 * 24 * 60 * 60 * 1000,
+  });
+};
 
 module.exports.signUp = async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
   try {
     const user = await UserModel.create({ email, password });
-    res.status(201).send("Le compte à bien été crée, veuillez vous connecter");
+    res
+      .status(201)
+      .send({ message: "Le compte à bien été crée, veuillez vous connecter" });
   } catch (err) {
-    res.status(420).send({ error: err });
+    res
+      .status(420)
+      .send({ message: "Vous possedez déjà un compte avec cet email" });
+  }
+};
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ token: token, userId: user._id });
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/signup", authController.signUp);
+router.post("/login", authController.signIn);
 
 module.exports = router; //
