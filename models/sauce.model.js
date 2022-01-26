8d2bf8e0a746311219212994d27cc77b4aaea573
jonathanm98 <jonathanmenelet@hotmail.fr>
2022-01-26 05:18:02 +0100

    Ajout Modèle de sauce

---
+++ models/sauce.model.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    manufacturer: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    mainPepper: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    heat: {
+      type: Number,
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+    usersLiked: {
+      type: [String],
+      required: true,
+    },
+    usersDisliked: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("sauce", sauceSchema);
+
+module.exports = UserModel;
